4eb2537452c21986389584b4c6e780f6c1932e67
Rogério de Sá <rogeriodesaf@gmail.com>
2024-04-30 19:01:39 -0300

    Criação do Projeto Minimal API

---
+++ Models/Heroes.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiSuperHeroes.Models
+{
+    public class Heroes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+    }
+}
