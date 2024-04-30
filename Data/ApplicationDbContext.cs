391a50dc91e97fe01d68f969af93f22fb8f328fb
Rogério de Sá <rogeriodesaf@gmail.com>
2024-04-30 19:24:18 -0300

    Criação das Migrations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiSuperHeroes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiSuperHeroes.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Heroes> Heroes { get; set; }   
+    }
+}
